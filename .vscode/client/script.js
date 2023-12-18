d6790da5be74b0a7fef89dbbbb836ced8a32a66c
Carollina-H <150289028+Carollina-H@users.noreply.github.com>
2023-12-18 10:56:13 +0100

    uppgift 7

---
+++ .vscode/client/script.js
@@ -0,0 +1,11 @@
+// Uppgift 6
+
+const url = "http://localhost:3000/users";
+fetch(url)
+  .then((response) => {
+    console.log(response);
+    return response.json();
+  })
+  .then((jsonData) => console.log(jsonData));
+
+// Uppgift 7
